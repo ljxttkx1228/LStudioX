57e931e05589d16c98fadeb53fea7a5898cd68e8
Moon <791235705@qq.com>
2025-12-02 19:30:54 +0800

    Initialize package.json for LStudio project

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "lstudio",
+  "version": "1.0.0",
+  "description": "LStudio 展示页面",
+  "scripts": {
+    "dev": "echo 'No dev server needed'",
+    "build": "echo 'No build step required'",
+    "start": "echo 'Static site'"
+  },
+  "keywords": ["lstudio", "portfolio"],
+  "author": "Your Name",
+  "license": "MIT"
+}
